754776219177ad6517fd03e7c044b4ed929c0b31
Mark Wilson <MW0901@proton.me>
2026-02-01 18:45:46 -0800

    Initial commit

---
+++ T766-Checkin/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace T766_Checkin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ T766-Checkin/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using T766_Checkin.Views;
+
+namespace T766_Checkin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = new WifiConnectView();
+        }
+
+        public void NavigateToNextPage()
+        {
+            MainContent.Content = new CheckinView();
+        }
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            this.Activate();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
